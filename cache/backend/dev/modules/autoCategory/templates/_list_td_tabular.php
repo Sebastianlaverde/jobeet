7a9a06105f1f1490fa1a8051ce70edbc19e2feb9
Sebastianlaverde <slaverdeiesv@gmail.com>
2024-06-17 17:54:17 -0500

    DELETE CACHE

--- cache/backend/dev/modules/autoCategory/templates/_list_td_tabular.php
+++
@@ -1,6 +0,0 @@
-<td data-label="Name" class="sf_admin_text sf_admin_list_td_name">
-  <?php echo link_to($jobeet_category->getName(), 'jobeet_category_edit', $jobeet_category) ?>
-</td>
-<td data-label="Slug" class="sf_admin_text sf_admin_list_td_slug">
-  <?php echo $jobeet_category->getSlug() ?>
-</td>
--- cache/backend/dev/modules/autoJob/templates/_assets.php
+++
@@ -1,3 +0,0 @@
- 
-<?php use_stylesheet('/sfDoctrinePlugin/css/global.css?v=10', 'first') ?> 
-<?php use_stylesheet('/sfDoctrinePlugin/css/default.css?v=10', 'first') ?> 
